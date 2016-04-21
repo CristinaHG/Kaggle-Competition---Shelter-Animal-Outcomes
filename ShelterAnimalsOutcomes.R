4bdbfbb5fa292afbd2f2aa32da05fd54c3bf24d3
CristinaHG <mrcrstnherediagmez@gmail.com>
2016-04-21 10:31:22 +0200

    reading sample submission csv

--- ShelterAnimalsOutcomes.R
+++ ShelterAnimalsOutcomes.R
@@ -1,2 +1,4 @@
 #Kaggle Competition-Shelter-Animal-Outcomes
 training <- read.csv('/home/cris/mrcrstnherediagmez@gmail.com/Kaggle_Comp/Kaggle-Competition---Shelter-Animal-Outcomes/train.csv')
+test <- read.csv('/home/cris/mrcrstnherediagmez@gmail.com/Kaggle_Comp/Kaggle-Competition---Shelter-Animal-Outcomes/test.csv')
+sample_submission <- read.csv('/home/cris/mrcrstnherediagmez@gmail.com/Kaggle_Comp/Kaggle-Competition---Shelter-Animal-Outcomes/sample_submission.csv')
